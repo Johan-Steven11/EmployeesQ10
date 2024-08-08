5689a8574d71a6bfd6ecde86402663a6031d8631
Steven <johan.salazar@belltech.la>
2024-08-08 04:30:50 -0500

    Subo proyecto a Git-Hub

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,121 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // Conexión a la base de datos utilizando el contexto de la entidad EmployeesEntities
+        EmployeesEntities entities = new EmployeesEntities();
+
+        // Método para listar todos los empleados de la base de datos
+        public ActionResult GetEmp()
+        {
+            entities.Configuration.ProxyCreationEnabled = false;  // Deshabilita la creación de proxies para evitar problemas de serialización
+
+            var getInfo = (from q in entities.Employees select q).ToList();  // Consulta para obtener todos los empleados
+            if (getInfo.Count > 0)
+            {
+                return Json(new { success = true, getInfo }, JsonRequestBehavior.AllowGet);  // Retorna los empleados si existen
+            }
+            else
+            {
+                return Json(new { success = true, message = "No se encontraron Datos" }, JsonRequestBehavior.AllowGet);  // Mensaje si no se encuentran empleados
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            return View();  // Retorna la vista para agregar o editar un empleado (formulario vacío o precargado)
+        }
+
+        // Método para agregar o editar empleados
+        [HttpPost]
+        public ActionResult AddOrEdit(Employees model)
+        {
+            entities.Configuration.ProxyCreationEnabled = false;  // Deshabilita la creación de proxies
+            string message = string.Empty;
+
+            if (ModelState.IsValid)
+            {
+                // Verifica si el empleado ya existe en la base de datos
+                var check = (from q in entities.Employees where q.ID == model.ID select q).FirstOrDefault();
+                if (check != null)
+                {
+                    // Si el empleado existe, se actualizan sus datos
+                    check.Nombre = model.Nombre;
+                    check.Position = model.Position;
+                    check.Office = model.Office;
+                    check.Salary = model.Salary;
+                    entities.Entry(check).State = EntityState.Modified;
+                    entities.SaveChanges();
+
+                    return Json(new { Exceptions = "Update", success = true, message = "Se actualizó un empleado" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    // Si el empleado no existe, se agrega uno nuevo
+                    Employees obj = new Employees();
+                    obj = model;
+                    entities.Employees.Add(obj);
+                    entities.SaveChanges();
+                    return Json(new { Exceptions = "Add", success = true, message = "Empleado Agregado" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+                return Json(new { success = false, message = "Por favor llena todos los espacios" }, JsonRequestBehavior.AllowGet);  // Mensaje de error si la validación del modelo falla
+        }
+
+        // Método para obtener la información de un empleado por su ID
+        public ActionResult GetEmployeeById(int Emp_id = 0)
+        {
+            entities.Configuration.ProxyCreationEnabled = false;  // Deshabilita la creación de proxies
+            var checkExist = (from q in entities.Employees where q.ID == Emp_id select q).FirstOrDefault();
+            if (checkExist != null)
+            {
+                return Json(checkExist, JsonRequestBehavior.AllowGet);  // Retorna los datos del empleado si existe
+            }
+            else
+            {
+                return Json("No se encontraron datos", JsonRequestBehavior.AllowGet);  // Mensaje si no se encuentra el empleado
+            }
+        }
+
+        // Método para eliminar un empleado por su ID
+        public ActionResult DeleteEmployee(int empID)
+        {
+            var checkInfo = (from q in entities.Employees where q.ID == empID select q).FirstOrDefault();
+
+            if (checkInfo != null)
+            {
+                // Intenta eliminar el empleado
+                try
+                {
+                    entities.Entry(checkInfo).State = EntityState.Deleted;
+                    entities.SaveChanges();
+                    return Json(new { success = true, message = "Un empleado fue eliminado" }, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception e)
+                {
+                    return Json(new { Exceptions = "Error", success = true, message = "Error: " + e.Message }, JsonRequestBehavior.AllowGet);  // Retorna un mensaje de error si la eliminación falla
+                }
+            }
+            else
+            {
+                return Json(new { Exceptions = "Error", success = false, message = "Error" }, JsonRequestBehavior.AllowGet);  // Mensaje si no se encuentra el empleado a eliminar
+            }
+        }
+
+        // Método de acción para mostrar la vista principal
+        public ActionResult Index()
+        {
+            return View();  // Retorna la vista de índice
+        }
+    }
+}
---
+++ EmployeeManagement/Scripts/JS/Scripts.js
@@ -0,0 +1,140 @@
+﻿// Función para listar los datos de los empleados en la tabla
+getEmployeeData()
+function getEmployeeData() {
+    $.ajax({
+        type: "GET", // Tipo de petición HTTP: GET
+        url: "/Employee/GetEmp", // URL a la que se hace la petición para obtener los datos de los empleados
+        data: {}, // No se envían datos adicionales en esta petición
+        success: function (data) { // Función que se ejecuta si la petición es exitosa
+            $("#tblBody").empty(); // Limpia el cuerpo de la tabla antes de agregar nuevos datos
+
+            if (data.getInfo.length > 0) { // Verifica si hay empleados en la respuesta
+                for (var i = 0; i < data.getInfo.length; i++) { // Itera sobre cada empleado en los datos obtenidos
+                    // Agrega una nueva fila a la tabla por cada empleado
+                    $("#tblBody").append("<tr>" +
+                        "<td>" + data.getInfo[i].ID + "</td>" +
+                        "<td>" + data.getInfo[i].Nombre + "</td>" +
+                        "<td>" + data.getInfo[i].Position + "</td>" +
+                        "<td>" + data.getInfo[i].Office + "</td>" +
+                        "<td>" + data.getInfo[i].Salary + "</td>" +
+                        "<td>" +
+                        "<a href='#' onclick='getEmployeeById(" + data.getInfo[i].ID + ")' class='btn btn-success'>Editar</a>" + // Botón para editar
+                        "<a onclick='DeleteEmployee(" + data.getInfo[i].ID + ")' href='#' class='btn btn-danger'>Eliminar</a>" + // Botón para eliminar
+                        "</td>" +
+                        "</tr>");
+                }
+            }
+        },
+        error: function (er) { // Función que se ejecuta si hay un error en la petición
+            alert(er); // Muestra un mensaje de alerta con el error
+        }
+    });
+}
+
+// Función para traer la información de un empleado por su ID y mostrarla en el formulario de edición
+function getEmployeeById(id) {
+    $("#EmployeeForm")[0].reset(); // Resetea el formulario de empleado
+    $.ajax({
+        type: "POST", // Tipo de petición HTTP: POST
+        url: "/Employee/GetEmployeeById", // URL a la que se hace la petición para obtener los datos de un empleado por su ID
+        data: {
+            Emp_id: id // Envía el ID del empleado como dato
+        },
+        success: function (data) { // Función que se ejecuta si la petición es exitosa
+            // Llena el formulario con los datos del empleado
+            $("#Emp_ID").val(data.ID);
+            $("#Nombre").val(data.Nombre);
+            $("#Position").val(data.Position);
+            $("#Office").val(data.Office);
+            $("#Salary").val(data.Salary);
+
+            // Actualiza el título del modal con el nombre del empleado
+            $("#exampleModalLabl").text("Empleado: " + data.Nombre);
+            $("#employeeModal").modal('show'); // Muestra el modal
+            $("#btnSaveEmp").addClass('btn-warning'); // Cambia el estilo del botón de guardar a advertencia (amarillo)
+            $("#btnSaveEmp").val('Actualizar Empleado'); // Cambia el texto del botón a "Actualizar Empleado"
+        },
+        error: function (er) { // Función que se ejecuta si hay un error en la petición
+            alert(er); // Muestra un mensaje de alerta con el error
+        }
+    });
+}
+
+// Función para eliminar un empleado por su ID
+function DeleteEmployee(id) {
+    var con = confirm("Desea eliminar este empleado "); // Muestra un cuadro de confirmación al usuario
+    if (con == true) { // Si el usuario confirma la eliminación
+
+        // Realiza la petición para eliminar al empleado
+        $.ajax({
+            type: "POST", // Tipo de petición HTTP: POST
+            url: "/Employee/DeleteEmployee", // URL a la que se hace la petición para eliminar al empleado
+            data: {
+                empID: id // Envía el ID del empleado a eliminar
+            },
+            success: function (data) { // Función que se ejecuta si la petición es exitosa
+                if (data.success == true) { // Si la eliminación fue exitosa
+                    if (data.Exceptions == "Error") { // Si hubo algún error durante el proceso
+                        alert(data.message); // Muestra el mensaje de error
+                    } else {
+                        getEmployeeData(); // Vuelve a cargar los datos de la tabla
+                        alert(data.message); // Muestra un mensaje indicando que el empleado fue eliminado
+                    }
+
+                } else {
+                    window.location.reload(); // Si hubo algún problema, recarga la página
+                }
+            },
+            error: function (er) { // Función que se ejecuta si hay un error en la petición
+                alert(er); // Muestra un mensaje de alerta con el error
+            }
+        });
+    }
+}
+
+// Validar que los inputs del formulario estén diligenciados y enviar los datos
+$("#btnSaveEmp").click(function () {
+    var oForm = document.forms["EmployeeForm"]; // Obtiene el formulario
+    if ($("#EmployeeForm").valid()) { // Verifica si el formulario es válido
+        var ajaxConfig = {
+            type: "POST", // Tipo de petición HTTP: POST
+            url: oForm.action, // URL a la que se envía el formulario
+            data: new FormData(oForm), // Datos del formulario que se enviarán
+            success: function (data) { // Función que se ejecuta si la petición es exitosa
+                if (data.success == true) { // Si la operación fue exitosa
+                    if (data.Exceptions == "Exist") { // Si ya existe un empleado con ese nombre
+                        $("#Nombre").val("").focus(); // Limpia el campo del nombre y le da foco
+                        alert(data.message); // Muestra el mensaje de error
+                    } else {
+                        getEmployeeData(); // Vuelve a cargar los datos de la tabla
+                        alert(data.message); // Muestra un mensaje indicando que la operación fue exitosa
+                        $("#employeeModal").modal('hide'); // Oculta el modal
+                    }
+                } else {
+                    alert(data.message); // Muestra un mensaje de error si la operación falló
+                }
+            },
+            error: function (er) { // Función que se ejecuta si hay un error en la petición
+                alert(er); // Muestra un mensaje de alerta con el error
+            }
+        };
+        if ($("form").attr('enctype') == "multipart/form-data") { // Verifica si el formulario tiene archivos adjuntos
+            ajaxConfig["contentType"] = false;
+            ajaxConfig["processData"] = false; // Configura la petición para manejar archivos
+        }
+        $.ajax(ajaxConfig); // Envía la petición AJAX
+        return false; // Evita que el formulario se envíe de manera tradicional
+    }
+});
+
+// Función para mostrar el modal y el formulario para agregar un nuevo empleado
+$("#AddNew").click(function () {
+    $("#EmployeeForm")[0].reset(); // Resetea el formulario
+    $("Emp_ID").val(0); // Resetea el ID del empleado en el formulario
+    $("btnSaveEmp").val("Add Employee"); // Cambia el valor del botón a "Add Employee"
+    $("#btnSaveEmp").removeClass("btn-warning"); // Elimina la clase de advertencia (amarillo) del botón
+    $(".modal-title").text("Agregar Nuevo Empleado"); // Cambia el título del modal
+    $("#employeeModal").modal('show'); // Muestra el modal
+    $("#btnSaveEmp").val('Agregar Empleado'); // Cambia el valor del botón a "Agregar Empleado"
+    $("#btnSaveEmp").addClass("btn-success"); // Agrega la clase de éxito (verde) al botón
+});
